1e8b1bdb3f09003bd13bf1f73d1dff1d74ab0a8b
tamatamvan <sfirst.tama@gmail.com>
2018-08-15 18:58:31 +0700

    adding postcss easy import and autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
 const lost = require('lost')
 const postcssNested = require('postcss-nested')
+const postcssEasyImport = require('postcss-easy-import')
+const autoprefixer = require('autoprefixer')
 
 module.exports = {
   plugins: [
+    postcssEasyImport({prefix: '_'}),
+    autoprefixer({}),
     lost({}),
     postcssNested({}),
   ]
