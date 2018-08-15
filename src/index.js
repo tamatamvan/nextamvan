429ba8f68293890f433affe77fc195e1b6470a89
tamatamvan <sfirst.tama@gmail.com>
2018-08-15 17:42:53 +0700

    initiate app inside src dir

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const Index = () => (
+  <div id="homepage">
+    <img src="/static/images/Programmer-cat.jpg" alt="I want to change the world"/>
+    <h1>Welcome to Nextamvan</h1>
+    <h2>A simple next.js boilerplate for your application</h2>
+
+    <style jsx>{`
+      #homepage {
+        padding: 3em;
+        text-align: center;
+      }
+    `}</style>
+  </div>
+)
+
+export default Index
