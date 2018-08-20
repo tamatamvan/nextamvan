98557ede5f332aa2d007a5cc4c0468419b73c524
tamatamvan <sfirst.tama@gmail.com>
2018-08-21 04:22:20 +0700

    basic navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,13 +7,7 @@ const Navbar = () => (
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
       <ul className="navbar-nav">
         <li className="nav-item active">
-          <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
+          <a className="nav-link" href="#">Home</a>
         </li>
       </ul>
     </div>
