5a916ca5dc8f8521627bc97139d129e94827912e
tamatamvan <sfirst.tama@gmail.com>
2018-08-21 03:44:59 +0700

    adding more meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,9 +11,17 @@ export default class MyDocument extends Document {
       <html lang="en">
         <Head>
           <title>Nextamvan</title>
+          <link rel="icon" href="/static/images/app-icons/nextamvan-32.png"/>
           <meta name="charset" content="utf-8"/>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="manifest" content="/static/manifest.json"/>
+          <meta name="theme-color" content="#2f2f2f" />
+          <link rel="shortcut icon" href="/static/images/app-icons/nextamvan-128.png" />
+          <link rel="apple-touch-icon" href="/static/images/app-icons/nextamvan-128.png" />
+          <meta name="apple-mobile-web-app-title" content="Nextamvan Boilerplate" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="mobile-web-app-capable" content="yes" />
         </Head>
         <body className="custom_class">
           <Main />
