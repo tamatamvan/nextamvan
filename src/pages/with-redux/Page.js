42cc3f90f1a02b78a727cfd5bf540ede12e161a2
Septian Tama <septian@cocowork.co>
2018-11-27 10:46:17 +0700

    fixing missed href props

--- src/pages/with-redux/Page.js
+++ src/pages/with-redux/Page.js
@@ -3,14 +3,14 @@ import { connect } from 'react-redux'
 import Clock from './Clock'
 import AddCount from './AddCount'
 
-const WithReduxExamplePage = (({ clockCounter: { title, linkTo, lastUpdate, light } }) => {
+const WithReduxExamplePage = (({ clockCounter: { title, lastUpdate, light } }) => {
   return (
     <div>
       <h1>{title}</h1>
       <Clock lastUpdate={ lastUpdate } light={light} />
       <AddCount />
       <nav>
-        <Link href={linkTo}><a>Back to Home</a></Link>
+        <Link href='/'><a>Back to Home</a></Link>
       </nav>
     </div>
   )
